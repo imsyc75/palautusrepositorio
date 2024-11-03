38cb8917962755848ee39f5c2ad3dff986dd21f7
imsyc75 <songyuchen2003@hotmail.com>
2024-11-03 23:32:58 +0200

    add test class

---
+++ viikko1/nhl-statistics-1/src/tests/test_statistics_service.py
@@ -0,0 +1,44 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri", "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(PlayerReaderStub())
+
+    def test_search_finds_player(self):
+        player = self.stats.search("Kurri")
+        self.assertIsNotNone(player)
+        self.assertEqual(player.name, "Kurri")
+
+    def test_search_returns_none_if_not_found(self):
+        player = self.stats.search("Nonexistent")
+        self.assertIsNone(player)
+
+    def test_team_returns_correct_players(self):
+        team_players = self.stats.team("EDM")
+        self.assertEqual(len(team_players), 3)
+        self.assertEqual(team_players[0].name, "Semenko")
+        self.assertEqual(team_players[1].name, "Kurri")
+        self.assertEqual(team_players[2].name, "Gretzky")
+
+    def test_top_returns_correct_number_of_players(self):
+        top_players = self.stats.top(3)
+        self.assertEqual(len(top_players), 3)
+        self.assertEqual(top_players[0].name, "Gretzky")
+        self.assertEqual(top_players[1].name, "Lemieux")
+        self.assertEqual(top_players[2].name, "Yzerman")
+
+if __name__ == '__main__':
+    unittest.main()
