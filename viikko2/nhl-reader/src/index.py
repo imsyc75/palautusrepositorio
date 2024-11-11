34e195f2b1a9edf08d4bc69a610fce99350cdca6
imsyc75 <songyuchen2003@hotmail.com>
2024-11-11 22:23:42 +0200

    ex13 graphic list

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,35 +1,33 @@
-import requests
-from player import Player
+from rich.console import Console
+from rich.table import Table
+from player import PlayerReader, PlayerStats
 
-class PlayerReader:
-    def __init__(self, url):
-        self.url = url
-
-    def get_players(self):
-        response = requests.get(self.url).json()
-        players = []
-        for player_dict in response:
-            player = Player(player_dict)
-            players.append(player)
-        return players
-
-class PlayerStats:
-    def __init__(self, player_reader):
-        self.players = player_reader.get_players()
-
-    def top_scorers_by_nationality(self, nationality):
-        filtered_players = filter(lambda p: p.nationality == nationality, self.players)
-        sorted_players = sorted(filtered_players, key=lambda p: p.total_points, reverse=True)
-        return sorted_players
-    
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    console = Console()
+
+    # get the season and nationality
+    season = console.input("Select season [2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25/]: ")
+    nationality = console.input("Select nationality [AUT/CZE/AUS/SWE/GER/DEN/SUI/SVK/NOR/RUS/CAN/LAT/BLR/SLO/USA/FIN/GBR/]: ")
+
+    url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+
+    players = stats.top_scorers_by_nationality(nationality)
+
+    # use the rich to show the result
+    table = Table(title=f"Top scorers of {nationality} season {season}")
+
+    table.add_column("name", justify="left")
+    table.add_column("team", justify="center")
+    table.add_column("goals", justify="center")
+    table.add_column("assists", justify="center")
+    table.add_column("points", justify="center")
 
     for player in players:
-        print(player)
+        table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.total_points))
+
+    console.print(table)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,3 +1,5 @@
+import requests
+
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
@@ -9,3 +11,24 @@ class Player:
     
     def __str__(self):
         return f"{self.name:20} {self.team:3} {self.goals:2} + {self.assists:2} = {self.total_points:3}"
+
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
+
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = []
+        for player_dict in response:
+            player = Player(player_dict)
+            players.append(player)
+        return players
+    
+class PlayerStats:
+    def __init__(self, player_reader):
+        self.players = player_reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        filtered_players = filter(lambda p: p.nationality == nationality, self.players)
+        sorted_players = sorted(filtered_players, key=lambda p: p.total_points, reverse=True)
+        return sorted_players
