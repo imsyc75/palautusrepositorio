25699591d931cd1273bae6cb3dd1b74e97eb590e
imsyc75 <songyuchen2003@hotmail.com>
2024-11-11 22:11:37 +0200

    ex12 refactor codes

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,25 +1,33 @@
 import requests
 from player import Player
 
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
 
-def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
-
-    print("JSON-muotoinen vastaus:")
-    print(response)
-
-    players = []
-
-    for player_dict in response:
-        if player_dict.get('nationality') == 'FIN': #finnish player
+    def get_players(self):
+        response = requests.get(self.url).json()
+        players = []
+        for player_dict in response:
             player = Player(player_dict)
             players.append(player)
-    
-    #sort by total points
-    players.sort(key=lambda p: p.total_points, reverse=True)
+        return players
 
-    print("\nPlayers from FIN:")
+class PlayerStats:
+    def __init__(self, player_reader):
+        self.players = player_reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        filtered_players = filter(lambda p: p.nationality == nationality, self.players)
+        sorted_players = sorted(filtered_players, key=lambda p: p.total_points, reverse=True)
+        return sorted_players
+    
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
+
     for player in players:
         print(player)
 
