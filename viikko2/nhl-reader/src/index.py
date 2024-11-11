fb2f2636c51b617ea7ecce0bece334cb39585eb3
imsyc75 <songyuchen2003@hotmail.com>
2024-11-11 21:57:08 +0200

    ex10 JSON list

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,25 @@
+import requests
+from player import Player
+
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    print(response)
+
+    players = []
+
+    for player_dict in response:
+        if player_dict.get('nationality') == 'FIN': #finnish player
+            player = Player(player_dict)
+            players.append(player)
+
+
+    print("\nPlayers from FIN:")
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
---
+++ viikko2/nhl-reader/src/player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, dict):
+        self.name = dict['name']
+        self.team = dict.get('team', 'N/A')
+        self.goals = dict.get('goals', 0)
+        self.assists = dict.get('assists', 0)
+        self.nationality = dict.get('nationality', 'N/A')
+    
+    def __str__(self):
+        return f"{self.name} team {self.team} goals {self.goals} assists {self.assists}"
+
