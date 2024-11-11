7a2a4c6f702cdfe81c6a7d49c244a17b5f01025f
imsyc75 <songyuchen2003@hotmail.com>
2024-11-11 20:46:30 +0200

    change tulo.py

--- viikko2/git-branch-harjoitus/tulo.py
+++ viikko2/git-branch-harjoitus/tulo.py
@@ -1,2 +1,15 @@
-def tulo(x, y):
-    return x * y
+from logger import logger
+from summa import summa
+from erotus import erotus
+from tulo import tulo
+
+logger("aloitetaan ohjelma")
+
+x = int(input("luku 1: "))
+y = int(input("luku 2: "))
+print(f"{x} + {y} = {summa(x, y)}") 
+print(f"{x} - {y} = {erotus(x, y)}") 
+print(f"{x} * {y} = {tulo(x, y)}") 
+
+logger("lopetetaan")
+print("goodbye!")
