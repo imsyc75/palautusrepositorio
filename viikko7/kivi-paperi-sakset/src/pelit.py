db0638d86c9f7752a5d8e98df26e1e6dd70b2a81
imsyc75 <songyuchen2003@hotmail.com>
2024-12-19 18:58:07 +0200

    viikko 7 exercise 3&4

---
+++ viikko7/kivi-paperi-sakset/src/pelit.py
@@ -0,0 +1,11 @@
+from kps_pelaaja_vs_pelaaja import KPSPelaajaVsPelaaja
+from kps_tekoaly import KPSTekoaly
+from kps_parempi_tekoaly import KPSParempiTekoaly
+
+def luo_peli(tyyppi):
+    pelit = {
+        'a': KPSPelaajaVsPelaaja,
+        'b': KPSTekoaly,
+        'c': KPSParempiTekoaly
+    }
+    return pelit.get(tyyppi)()
